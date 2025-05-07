ee78cacc8bf431b5b90f1ea303e3d649a52dc73c
Tanawan (Juneau) Chatchadanoraset <tchatc24@uw.edu>
2025-05-07 10:43:25 -0700

    Deprecate tests

--- test/test_basic.jl
+++ test/test_basic.jl
@@ -24,10 +24,15 @@ end
 
     es = [0.0001, 0.1, 0.5, 0.9]
 
+    # ωs = [0., π/2, π, -π/2, 1e-4, -1e-4]
+    # es = [0.0001, 0.1, 0.5, 0.9]
+    # Is = [0., 1., π/2]
+    # Ωs = [0., 1., π/2, -π/2]
+
     for ω in ωs, e in es
         p1 = Elements(m=1)
-        p2 = Elements(m=1e-6, P=365.242, e=e, ω=ω)
-        p3 = Elements(m=1e-6, P=2*365.242, e=e, ω=ω)
+        p2 = Elements(m=1e-4, P=365.242, e=e, ω=ω, I=π/2, Ω=0)
+        p3 = Elements(m=1e-4, P=2*365.242, e=e, ω=ω, I=π/2, Ω=0)
 
 
         ic = ElementsIC(0., 3, p1, p2, p3)
@@ -40,41 +45,41 @@ end
             pre_elems = ic.elements[i,:]
             elems = get_orbital_elements(s, ic)[i]
             post_elems = [elems.m, elems.P, elems.t0, elems.e * cos(elems.ω), elems.e * sin(elems.ω), elems.I, elems.Ω]
-            @test isapprox(pre_elems, post_elems; atol=1e-10)
+            @test isapprox(pre_elems, post_elems; atol=1e-8)
         end
     end
 end
 
-@testset "Orbit initialization" begin
-    ωs = [0., π/2, π, -π/2, 1e-4, -1e-4]
-    es = [0.0001, 0.1, 0.5, 0.9]
-    Is = [0., 1., π/2]
-    Ωs = [0., 1., π/2, -π/2]
+# @testset "Orbit initialization" begin
+#     ωs = [0., π/2, π, -π/2, 1e-4, -1e-4]
+#     es = [0.0001, 0.1, 0.5, 0.9]
+#     Is = [0., 1., π/2]
+#     Ωs = [0., 1., π/2, -π/2]
 
-    for ω in ωs, e in es, I in Is, Ω in Ωs
-        p1 = Elements(m=1)
-        p2 = Elements(m=1e-4, P=365.242, e=e, ω=ω, I=I, Ω=Ω)
-        p3 = Elements(m=1e-4, P=2*365.242, e=e, ω=ω, I=I, Ω=Ω)
+#     for ω in ωs, e in es, I in Is, Ω in Ωs
+#         p1 = Elements(m=1)
+#         p2 = Elements(m=1e-4, P=365.242, e=e, ω=ω, I=I, Ω=Ω)
+#         p3 = Elements(m=1e-4, P=2*365.242, e=e, ω=ω, I=I, Ω=Ω)
 
-        ic = ElementsIC(0., 3, p1, p2, p3)
-        s = State(ic)
+#         ic = ElementsIC(0., 3, p1, p2, p3)
+#         s = State(ic)
 
-        o = Orbit(s, ic, 2.001)
+#         o = Orbit(s, ic, 2.001)
 
-        init_from_M!(o.s, o.ic, 0., 2)
-        init_from_M!(o.s, o.ic, 0., 3)
+#         init_from_M!(o.s, o.ic, 0., 2)
+#         init_from_M!(o.s, o.ic, 0., 3)
 
-        anoms = get_anomalies(o.s, o.ic)
+#         anoms = get_anomalies(o.s, o.ic)
 
-        println("Testing : ω = $ω, e = $e, I = $I, Ω= $Ω")
-        for i=1:o.nplanet
-            test_anom = anoms[i][2] - π
-            if test_anom >= 0
-                test_anom = abs(test_anom - π)
-            else
-                test_anom = abs(test_anom + π)
-            end
-            @test isapprox(test_anom, 0.; atol=1e-3)
-        end
-    end
-end
+#         println("Testing : ω = $ω, e = $e, I = $I, Ω= $Ω")
+#         for i=1:o.nplanet
+#             test_anom = anoms[i][2] - π
+#             if test_anom >= 0
+#                 test_anom = abs(test_anom - π)
+#             else
+#                 test_anom = abs(test_anom + π)
+#             end
+#             @test isapprox(test_anom, 0.; atol=1e-3)
+#         end
+#     end
+# end
