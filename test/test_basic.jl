827f6c75b8b1a6e008eb1a316f94afd59468078f
Tanawan (Juneau) Chatchadanoraset <tchatc24@uw.edu>
2025-05-09 13:55:02 -0700

    Delete old tests

--- test/test_basic.jl
+++ test/test_basic.jl
@@ -68,37 +68,3 @@ end
         @test isapprox(anoms[i][2], vec[3+i]; atol=1e-8)
     end
 end
-
-# @testset "Orbit initialization" begin
-#     ωs = [0., π/2, π, -π/2, 1e-4, -1e-4]
-#     es = [0.0001, 0.1, 0.5, 0.9]
-#     Is = [0., 1., π/2]
-#     Ωs = [0., 1., π/2, -π/2]
-
-#     for ω in ωs, e in es, I in Is, Ω in Ωs
-#         p1 = Elements(m=1)
-#         p2 = Elements(m=1e-4, P=365.242, e=e, ω=ω, I=I, Ω=Ω)
-#         p3 = Elements(m=1e-4, P=2*365.242, e=e, ω=ω, I=I, Ω=Ω)
-
-#         ic = ElementsIC(0., 3, p1, p2, p3)
-#         s = State(ic)
-
-#         o = Orbit(s, ic, 2.001)
-
-#         init_from_M!(o.s, o.ic, 0., 2)
-#         init_from_M!(o.s, o.ic, 0., 3)
-
-#         anoms = get_anomalies(o.s, o.ic)
-
-#         println("Testing : ω = $ω, e = $e, I = $I, Ω= $Ω")
-#         for i=1:o.nplanet
-#             test_anom = anoms[i][2] - π
-#             if test_anom >= 0
-#                 test_anom = abs(test_anom - π)
-#             else
-#                 test_anom = abs(test_anom + π)
-#             end
-#             @test isapprox(test_anom, 0.; atol=1e-3)
-#         end
-#     end
-# end
