edee8de27b35df5ede28d0be6bb31eefdc20b224
Tanawan (Juneau) Chatchadanoraset <tchatc24@uw.edu>
2025-05-16 23:51:14 -0700

    Show orbital element difference

--- test/test_optimization.jl
+++ test/test_optimization.jl
@@ -29,6 +29,9 @@ optres = find_periodic_orbit(optparams, orbparams)
 # Orbit object takes three arguments: number of planets, opt params, and orbit params
 o = Orbit(4, optres, orbparams)
 
+initial_elements = get_orbital_elements(o.s, o.ic)
+initial_anomalies = get_anomalies(o.s, o.ic)
+
 # Rebound visualization
 sim = rebound.Simulation()
 sim.add(m=1)
@@ -40,11 +43,17 @@ end
 rebound.OrbitPlot(sim, projection="xz")
 plt.title("Before")
 
+# Make sure we are using a new Orbit object
+o = Orbit(4, optres, orbparams)
+
+# Integration
 step_size = 0.1 * optres.inner_period
 ctime = 0.0
-
 intr = Integrator(ahl21!, 10., orbparams.tsys)
 intr(o.s)
+
+final_elements = get_orbital_elements(o.s, o.ic)
+final_anomalies = get_anomalies(o.s, o.ic)
 
 # Rebound visualization
 sim = rebound.Simulation()
@@ -56,3 +65,18 @@ end
 
 rebound.OrbitPlot(sim, projection="xz")
 plt.title("After")
+
+println("\nDifferences (final - initial):")
+for i in 1:(o.nplanet) 
+    println("  Planet $(i):")
+    println("    Semi-major axis diff: ", final_elements[i+1].a - initial_elements[i+1].a)
+    println("    Eccentricity diff: ", final_elements[i+1].e - initial_elements[i+1].e)
+    println("    Argument of perihelion diff: ", rem2pi(final_elements[i+1].ω - initial_elements[i+1].ω, RoundNearest))
+    println("    Mean anomaly diff: ", rem2pi(final_anomalies[i][2] - initial_anomalies[i][2], RoundNearest))
+    
+    if i >= 2
+        rel_ω_initial = initial_elements[i+1].ω - initial_elements[i].ω
+        rel_ω_final = final_elements[i+1].ω - final_elements[i].ω
+        println("    Relative orientation diff: ", rem2pi(rel_ω_final - rel_ω_initial, RoundNearest))
+    end
+end
