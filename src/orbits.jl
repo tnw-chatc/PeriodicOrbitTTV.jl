774be1f62205255c02f59f8f0730a695372fee12
Tanawan (Juneau) Chatchadanoraset <tchatc24@uw.edu>
2025-05-16 21:53:28 -0700

    Remove deprecated two-planet system optimization function

--- src/orbits.jl
+++ src/orbits.jl
@@ -183,39 +183,6 @@ Orbit(n::Int, optparams::OptimParameters{T}, orbparams::OrbitParameters{T}) wher
     Orbit(s, ic, orbparams.κ)
 end
 
-"""Will remove soon"""
-function optimize!(optparams::OptimParameters{T}) where T <: AbstractFloat
-    # Target mean anomaly for two planet system is 4π
-    orbit = Orbit(2, optparams, 2.00)
-
-    target_M = 4π
-    target_time, times, Ms = integrate_to_M!(orbit.s, orbit.ic, target_M, 1);
-
-    orbit.s = State(orbit.ic)
-    intr = Integrator(0.5, 0., target_time)
-    intr(orbit.s)
-
-    # TODO: Implement the actual optimization for N-body system (currently only for 2 planets)
-    # TODO: Make sure the angle wrapping works as intended
-    final_e1 = get_orbital_elements(orbit.s, orbit.ic)[2].e
-    final_e2 = get_orbital_elements(orbit.s, orbit.ic)[3].e
-    final_M = get_anomalies(orbit.s, orbit.ic)[1][2]
-    final_Δω = get_orbital_elements(orbit.s, orbit.ic)[3].ω - get_orbital_elements(orbit.s, orbit.ic)[2].ω
-
-    final_optparams = [final_e1, final_e2, rem2pi(final_M, RoundNearest), rem2pi(final_Δω, RoundNearest)]
-
-    # Init params
-    init_optparams = tovector(optparams)
-    
-    # Wrap angles
-    init_optparams[3] = rem2pi(init_optparams[3], RoundNearest)
-    init_optparams[4] = rem2pi(init_optparams[4], RoundNearest)
-
-    diff = final_optparams .- init_optparams
-
-    return sum(diff.^2)
-end
-
 Base.show(io::IO,::MIME"text/plain",o::Orbit{T}) where {T} = begin
     println("Orbit")
     println("Number of planets: $(o.ic.nbody-1)")
