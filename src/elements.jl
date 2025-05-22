070a36ab709d3c934473f0933223d65379a2f2e4
Hurum Maksora Tohfa <ttohfa@brynmawr.edu>
2025-05-21 22:39:43 -0700

    Update convert_to_elements

--- src/elements.jl
+++ src/elements.jl
@@ -12,85 +12,61 @@ function point2vector(x::NbodyGradient.Point)
     return [x.x, x.y, x.z]
 end
 
+using LinearAlgebra
+
 function convert_to_elements(x::Vector{T}, v::Vector{T}, Gm::T, t::T) where T <: AbstractFloat
-    # (2.126) R^2 = x^2 + y^2 + z^2
-    r = mag(x)
-    # (2.127) V^2 = vx^2 + vy^2 + vz^2
+    R = norm(x)
     v2 = dot(v, v)
-    vmag = mag(v) # Magnitude of velocity
-    # (2.129) h = r × v
     hvec = cross(x, v)
-    h = mag(hvec)
-    # Specific orbital energy (2.28): ε = v^2/2 - Gm/r
-    ε = 0.5 * v2 - Gm / r
-    # (2.134) a = [2/r - v^2 / Gm]^-1
-    if abs(ε) < 1e-10
-        # Parabolic
-        a = Inf
-        e = 1.0
-    else
-        a = -Gm / (2ε)
-        # e = sqrt(1 - h^2 / (Gm * a)) # (2.135) 
-        e = sqrt(1.0 + 2ε * h^2 / Gm^2)
-    end
-    # (2.131) inclination I = arccos(hz / h)
-    # hx, hy, hz = unpack(hvec)
-    hx, hy, hz = hvec
-    I = acos(clamp(hz / h, -1, 1))
-    # (2.132-2.133) Ω from hvec
-    Ω = I > 1e-8 ? atan(hx, -hy) : 0.0
-    evec = (cross(v, hvec) / Gm) .- (x / r)
-    ω = 0.0
-    if I > 1e-8
-        nvec = [-hy, hx, 0.0]
-        n = mag(nvec)
-        if n > 0
-            ω = acos(clamp(dot(nvec, evec) / (n * e), -1, 1))
-            if evec[3] < 0
-                ω = 2π - ω
-            end
-        end
-    else
-        ω = atan(evec[2], evec[1])
-        if evec[3] < 0
-            ω = 2π - ω
-        end
-    end
-    # (2.20) true anomaly f = angle between evec and r
-    cosf = clamp(dot(evec, x) / (e * r), -1, 1)
-    sinf = dot(x, v) / (e * sqrt(Gm * a))
+    h = norm(hvec)
+
+    # (2.134)
+    a = [1 / 2/R - v^2/Gm]^-1
+
+    # (2.135)
+    e = sqrt(1 - h^2 / (Gm * a))
+
+    # (2.136)
+    I = acos(clamp(hvec[3] / h, -1, 1))
+
+    # (2.137) -check sign convention with eric
+    sinΩ = hvec[1] / (h * sin(I))
+    cosΩ = -hvec[2] / (h * sin(I))
+    Ω = atan(sinΩ, cosΩ)
+
+    # (2.138)
+    sin_ω_plus_f = x[3] / (R * sin(I))
+    cos_ω_plus_f = (1 / cos(Ω)) * (x[1] / R + sin(Ω) * sin_ω_plus_f * cos(I))
+    ω_plus_f = atan(sin_ω_plus_f, cos_ω_plus_f)
+
+    # (2.139) assuming Rdot = (x.v)/R which is projection of velocity in radial direction
+    sinf = a * (1 - e^2) * dot(x, v) / (h * e * R)
+    cosf = (1 / e) * (a * (1 - e^2) / R - 1)
     f = atan(sinf, cosf)
-    # (2.42) r = a(1 - e cosE)
-    # (2.50, 2.51) Eccentric anomaly and mean anomaly
-    E = 0.0
-    M = 0.0
-    if e < 1.0
-        E = 2 * atan( sqrt((1 - e) / (1 + e)) * tan(f / 2) )
-        M = E - e * sin(E)
-    elseif e ≈ 1.0
-        M = 0.0
-    else
-        # Hyperbolic orbit
-        F = asinh( sqrt((e - 1) / (1 + e)) * tanh(f / 2) )
-        M = e * sinh(F) - F
-    end
-    # (2.39) and (2.140)  mean anomaly , time of periapsis
-    n = sqrt(Gm / abs(a^3))
-    τ = t - M / n
+
+    ω = ω_plus_f - f
+
+    # (2.140)
+    E = 2 * atan(sqrt((1 - e) / (1 + e)) * tan(f / 2))
+    M = E - e * sin(E)
+    n_mean = sqrt(Gm / a^3)
+    τ = t - M / n_mean
+
     return (
-        a=a,
-        e=e,
-        I=I,
-        Ω=mod2pi(Ω),
-        ω=mod2pi(ω),
-        f=mod2pi(f),
-        M=mod2pi(M),
-        E=mod2pi(E),
-        τ=τ,
-        n=n,
-        h=h
+        a = a,
+        e = e,
+        I = I,
+        Ω = mod2pi(Ω),
+        ω = mod2pi(ω),
+        f = mod2pi(f),
+        M = mod2pi(M),
+        E = mod2pi(E),
+        τ = τ,
+        n = n_mean,
+        h = h
     )
 end
+
 
 function normvec(x::Vector{T}, v::Vector{T}, Gm::T) where T <: AbstractFloat
     # TODO: Need more comprehensive testing
