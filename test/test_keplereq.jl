8f239fec8a1392905e0b76415a5eba62560b793b
Tanawan (Juneau) Chatchadanoraset <tchatc24@uw.edu>
2025-07-10 10:29:14 -0700

    Add a test for Kepler solver function

---
+++ test/test_keplereq.jl
@@ -0,0 +1,24 @@
+using PeriodicOrbit
+using PeriodicOrbit: solve_kepler_equation
+
+using Test
+
+epsilon = eps(Float64)
+
+@testset "Kepler Equation Solver" begin
+    M_cands = [0., π/2, 1π, 3π/2,
+    0. + 1e-4, π/2 + 1e-4, 1π + 1e-4, 3π/2 + 1e-4,
+    0. - 1e-4, π/2 - 1e-4, 1π - 1e-4, 3π/2 - 1e-4,
+    0., -π/2, -1π, -3π/2,
+    ]
+
+    e_cands = [0.1, 0.3, 0.5, 0.9]
+
+    for M in M_cands, e in e_cands
+        println("Testing: $M, $e")
+        E = solve_kepler_equation(M, e)
+        
+        println("$M, $(E - e * sin(E))")
+        @test isapprox(M, E - e * sin(E); rtol=epsilon)
+    end
+end
