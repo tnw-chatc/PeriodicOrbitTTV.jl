587975b32a5555949f1b204f39968d563f3e8856
Tanawan (Juneau) Chatchadanoraset <tchatc24@uw.edu>
2025-07-10 15:57:36 -0700

    Lower the tolerances of the solver and test
    Lower tolerance of the test

--- test/test_keplereq.jl
+++ test/test_keplereq.jl
@@ -3,8 +3,6 @@ using PeriodicOrbit: solve_kepler_equation
 
 using Test
 
-epsilon = eps(Float64)
-
 @testset "Kepler Equation Solver" begin
     M_cands = [0., π/2, 1π, 3π/2,
     0. + 1e-4, π/2 + 1e-4, 1π + 1e-4, 3π/2 + 1e-4,
@@ -12,13 +10,13 @@ epsilon = eps(Float64)
     0., -π/2, -1π, -3π/2,
     ]
 
-    e_cands = [0.0, 0.0001, 0.1, 0.5, 0.9, 0.99999]
+    e_cands = [0.0, 0.0001, 0.1, 0.5, 0.9, 0.9999]
 
     for M in M_cands, e in e_cands
         println("Testing: $M, $e")
-        E = solve_kepler_equation(M, e)
+        E = solve_kepler_equation(M, e; tol=1e-14)
         
         println("$M, $(E - e * sin(E))")
-        @test isapprox(M, E - e * sin(E); rtol=epsilon)
+        @test isapprox(M, E - e * sin(E); rtol=1e-12)
     end
 end
